0f8c00815fecbb5a3abeea3235cdb029d003a43f
truth1984 <zhengyao365@163.com>
2020-01-02 16:43:25 +0800

    update to 1.6.6

--- index.js
+++ index.js
@@ -29,14 +29,41 @@ module.exports = class Pool {
       .fill()
       .map((i, index) => index);
     this.entry._posterString = `let post = (data, type = "msg") => parentPort.postMessage({ data, type }); 
-    console.log = (...data) => post(data);`;
+    console={log:(...data) => post(data),warn:(...data) => post(data,"msg-warn"),error:(...data) => post(data,"msg-error")};
+    `;
+
+    this.entry.workerMaker = () =>
+      new Worker(
+        ` const {  parentPort } = require("worker_threads");
+        ${this.entry.importGlobal} 
+        ${this.entry._posterString}
+
+        let evaluate = item => {
+          let func = eval(item.func);
+          let result = func(...item.param); 
+          if (result instanceof Promise){
+            result.then(data => post(data, "result")).catch(error => post(error, "reject")).then(()=>process.exit())
+          } else {
+            post(result, "result");
+            process.exit();
+          }
+        };
+
+        parentPort.once("message", message => {
+          if (message.type == "eval") return evaluate(message);
+        });
+        `,
+        { eval: true, env: SHARE_ENV }
+      );
 
     this.entry.setListener = (worker, resolve, reject) => {
       worker.removeAllListeners();
       worker.on("message", message => {
-        if (message.type == "msg") console.log(...message.data);
-        else if (message.type == "reject") reject(message.data);
-        else resolve(message.data);
+        if (message.type == "msg") return console.log(...message.data);
+        if (message.type == "result") return resolve(message.data);
+        if (message.type == "reject") return reject(message.data);
+        if (message.type == "msg-warn") return console.warn(...message.data);
+        if (message.type == "msg-error") return console.error(...message.data);
       });
       worker.once("error", error => reject(error));
       worker.once("exit", () => resolve());
@@ -49,22 +76,9 @@ module.exports = class Pool {
    */
   threadSingleStoppable(func, ...param) {
     if (isMainThread) {
-      let worker = new Worker(
-        ` const {  parentPort, workerData } = require("worker_threads");
-          ${this.entry.importGlobal} 
-          ${this.entry._posterString}
-      
-          let result = (${func.toString()})(...workerData.parameter); 
+      let worker = this.entry.workerMaker();
+      worker.postMessage({ func: func.toString(), param, type: "eval" });
 
-          if (result instanceof Promise){
-            result.then(data => post(data, "result")).catch(error => post(error, "reject")).then(()=>process.exit())
-          } else {
-            post(result, "result");
-            process.exit();
-          }
-          `,
-        { eval: true, workerData: { parameter: param }, env: SHARE_ENV }
-      );
       return {
         cancel: () => worker.terminate(),
         result: new Promise((resolve, reject) => this.entry.setListener(worker, resolve, reject))
@@ -83,52 +97,6 @@ module.exports = class Pool {
     return this.threadSingleStoppable(func, ...param).result;
   }
 
-  _threadAlive(id, func, ...param) {
-    if (isMainThread) {
-      if (!this.entry._threadPools[id]) {
-        this.entry._threadPools[id] = new Worker(
-          ` const { parentPort, workerData } = require("worker_threads");
-            ${this.entry.importGlobal} 
-            ${this.entry._posterString}
-        
-            parentPort.on('message', item => {
-              let func = eval(item.func);
-              let result = func(...item.param); 
-              result instanceof Promise
-                ? result.then(data => post(data, "result")).catch(error => post(error, "reject"))
-                : post(result, "result");
-            });
-            `,
-          { eval: true, env: SHARE_ENV }
-        );
-      }
-      this.entry._threadPools[id].postMessage({ func: func.toString(), param });
-
-      let publisher = data => {
-        this.entry._threadAvailableID.push(id);
-        return data;
-      };
-
-      let result = new Promise((resolve, reject) =>
-        this.entry.setListener(this.entry._threadPools[id], resolve, reject)
-      )
-        .then(data => publisher(data))
-        .catch(error => Promise.reject(publisher(error)));
-
-      return {
-        cancel: () => {
-          this.entry._threadPools[id].terminate();
-          delete this.entry._threadPools[id];
-        },
-        result
-      };
-    }
-    return {
-      cancel: () => {},
-      result: Promise.reject("This is not in the main thread")
-    };
-  }
-
   async threadPool(func, ...param) {
     return this.threadPoolStoppable(func, ...param)
       .then(data => data.result)
@@ -144,6 +112,30 @@ module.exports = class Pool {
       return this.threadPoolStoppable(func, ...param);
     }
     let threadid = this.entry._threadAvailableID.pop();
-    return this._threadAlive(threadid, func, ...param);
+    if (isMainThread) {
+      if (!this.entry._threadPools[threadid]) this.entry._threadPools[threadid] = this.entry.workerMaker();
+      this.entry._threadPools[threadid].postMessage({ func: func.toString(), param, type: "eval" });
+
+      let publisher = data => {
+        this.entry._threadAvailableID.push(threadid);
+        return data;
+      };
+
+      return {
+        cancel: () => {
+          this.entry._threadPools[threadid].terminate();
+          delete this.entry._threadPools[threadid];
+        },
+        result: new Promise((resolve, reject) =>
+          this.entry.setListener(this.entry._threadPools[threadid], resolve, reject)
+        )
+          .then(data => publisher(data))
+          .catch(error => Promise.reject(publisher(error)))
+      };
+    }
+    return {
+      cancel: () => {},
+      result: Promise.reject("This is not in the main thread")
+    };
   }
 };
